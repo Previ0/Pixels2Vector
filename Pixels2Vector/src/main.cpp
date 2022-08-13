3049be2e11b0c0b294fb2cf4b8278fcbe6ddd065
Previ0 <tuckipatryk@gmail.com>
2022-08-13 16:52:36 +0200

    File extension handler

--- Pixels2Vector/src/main.cpp
+++ Pixels2Vector/src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <bitset>
+#include <algorithm>
 #include <fstream>
 #include "image.h"
 #include "string"
 
+void setExtensionName(std::string& filename, std::string expected);
 
 int main(int argc, char* argv[]) {
 
@@ -19,6 +21,7 @@ int main(int argc, char* argv[]) {
 	std::string filename;
 	std::cout << "Filename: ";
 	std::getline(std::cin, filename);
+	setExtensionName(filename, ".bmp");
 
 	std::ifstream file(filename, std::ios::binary);
 	Image image(file);
@@ -28,4 +31,38 @@ int main(int argc, char* argv[]) {
 	std::cin.get();
 
 	return 0;
+}
+
+void setExtensionName(std::string& filename, std::string expected) {
+	std::string temp;
+
+	// expected extension to lowercase
+	for (char c : expected) {
+		std::tolower(c);
+	}
+
+	// checking wheter extension name has starting character '.'
+	if (expected[0] != '.')
+		expected = "." + expected;
+
+	// if filename is empty, it 'creates example' name
+	if (filename.length() == 0) {
+		filename = "example" + expected;
+		return;
+	}
+
+	// checking if filename contains file extension name
+	int i = filename.length() - 1;
+	while (filename[i] != '.' && i >= 0) {
+		temp.push_back(std::tolower(filename[i]));
+		--i;
+	}
+	temp.push_back('.');
+	std::reverse(temp.begin(), temp.end());
+
+	// checking if filename's extension is the same as expected
+	// if not, it adds file extension name
+	if (temp != expected) filename += expected;
+	std::cout << "temp: " << temp << std::endl;
+
 }
